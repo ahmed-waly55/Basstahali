78bc160e7ee95721084d1ad7170eee4c11d931ea
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-07-29 02:29:24 +0300

    add about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     // Landing
     {path:"", redirectTo:"/home", pathMatch:"full"},
-    {path: "home", loadComponent:()=> import("./layouts/landing-layout/landing-layout.component").then(m => m.LandingLayoutComponent), title:"بسطهالي | منصة تعليمية ذكية لشرح المناهج الدراسية", 
+    {path: "", loadComponent:()=> import("./layouts/landing-layout/landing-layout.component").then(m => m.LandingLayoutComponent), title:"بسطهالي | منصة تعليمية ذكية لشرح المناهج الدراسية", 
     children:[
-        {path:"", loadComponent:()=> import("./Features/landing/landing.component").then(m => m.LandingComponent)}
+        {path:"home", loadComponent:()=> import("./Features/landing/landing.component").then(m => m.LandingComponent)},
+        {path:"about", loadComponent:()=> import('./Features/about/about.component').then(m => m.AboutComponent)}
     ]},
 
     {path:"**" , loadComponent:()=> import("./shared/not-found/not-found.component").then(m => m.NotFoundComponent)}
