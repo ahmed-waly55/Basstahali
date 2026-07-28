316b4ae57fc0bc81118ab51b76a8f773bf793ed1
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-07-28 15:59:06 +0300

    hero landing page

--- src/app/Features/landing/landing.component.ts
+++ src/app/Features/landing/landing.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from "../hero/hero.component";
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [HeroComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css',
 })
