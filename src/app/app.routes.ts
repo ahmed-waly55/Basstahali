cc444b0d1e49853dd9a1765cab84e3a0c75a8555
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-08-03 13:40:57 +0300

    add login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     // Landing
     {path:"", redirectTo:"/home", pathMatch:"full"},
-    {path: "", loadComponent:()=> import("./layouts/landing-layout/landing-layout.component").then(m => m.LandingLayoutComponent), title:"بسطهالي | منصة تعليمية ذكية لشرح المناهج الدراسية", 
+    {path: "", loadComponent:()=> import("./layouts/landing-layout/landing-layout.component").then(m => m.LandingLayoutComponent), title:"بسطهالي | منصة تعليمية ذكية لشرح المناهج الدراسية",
     children:[
         {path:"home", loadComponent:()=> import("./Features/landing/landing.component").then(m => m.LandingComponent)},
         {path:"about", loadComponent:()=> import('./Features/about/about.component').then(m => m.AboutComponent)}
     ]},
+
+    {path:"auth", loadComponent:()=> import("./layouts/auth-layout/auth-layout.component").then(m => m.AuthLayoutComponent), title:"بسطهالي | تسجيل الدخول",
+    children:[
+        {path:"login", loadComponent:()=> import("./Features/auth/login/login.component").then(m => m.LoginComponent)},
+    ]},
+
 
     {path:"**" , loadComponent:()=> import("./shared/not-found/not-found.component").then(m => m.NotFoundComponent)}
 
